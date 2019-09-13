a3773e3d68aa694acf7cb5df777d27be7378e6ff
Megan White <mfw247@gmail.com>
2019-09-13 08:55:51 -0700

    set up migration file

--- data/migrations/20190913084104_create-projects.js
+++ data/migrations/20190913084104_create-projects.js
@@ -1,8 +1,44 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable("projects", tbl => {
+            tbs.increments();
+            tbl.string("name",128).notNullable();
+            tbl.text("description");
+            tbl.boolean("completed").notNullable().defaultTo(false)
+        })
+        .createTable("resources", tbl => {
+            tbl.increments();
+            tbl.string("name",128).unique().notNullable();
+            tbl.text("description")
+        })
+        .createTable("tasks", tbl => {
+            tbl.increments();
+            tbl.text("description");
+            tbl.text("notes");
+            tbl.boolean("completed").notNullable().defaultTo(false);
+        })
+        .createTable("project_resources",tbl => {
+            tbl
+                .integer("project_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("projects")
+            tbl
+                .integer("resource_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("resources")
+            tbl.primary(["project_id","resource_id"])
+        }); 
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists("project_resources")
+        .dropTableIfExists("tasks")
+        .dropTableIfExists("resources")
+        .dropTableIfExists("projects")
 };
