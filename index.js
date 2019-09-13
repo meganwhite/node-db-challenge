03200281127d92957b3a3d88d155b6f448d54e2a
Megan White <mfw247@gmail.com>
2019-09-13 08:45:14 -0700

    debugged sever setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require("./server.js");
+const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 6000;
 
 server.listen(PORT, () => {
-    console.log(`\n** Server listening on port ${port} **\n`)
+    console.log(`\n** Server listening on port ${PORT} **\n`)
 })
